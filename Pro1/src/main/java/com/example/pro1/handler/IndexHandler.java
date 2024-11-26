1af27598c9844e82514a4248ddc1d8a4872fb547
ANEWONEONLY <1354341210@qq.com>
2024-11-26 08:57:31 +0800

    TEST

---
+++ Pro1/src/main/java/com/example/pro1/handler/IndexHandler.java
@@ -0,0 +1,16 @@
+package com.example.pro1.handler;
+
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+
+@Path("/")
+public class IndexHandler {
+
+    @GET
+    public String index() {
+        return "Hello World!";
+    }
+}
+
+
